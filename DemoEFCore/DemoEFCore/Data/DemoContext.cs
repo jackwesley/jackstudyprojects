52712ceb8cdc219f9fa8dfdf4ba68212433087a0
Jack Wesley Moreira <jackwesley@hotmail.com>
2017-06-14 21:35:29 -0300

    [DemoEFCore]

---
+++ DemoEFCore/DemoEFCore/Data/DemoContext.cs
@@ -0,0 +1,29 @@
+﻿using DemoEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoEFCore.Data
+{
+    public class DemoContext : DbContext
+    {
+
+
+        public DemoContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DemoContext()
+               : base()
+        {
+
+        }
+
+ 
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
---
+++ DemoEFCore/DemoEFCore/Models/Evento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoEFCore.Models
+{
+    public class Evento
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O Nome é obrigatorio")]
+        [Display(Name = "Nome Completo")]
+        [MaxLength(100, ErrorMessage = "O valor máximo é 100")]
+        [MinLength(2, ErrorMessage = "O valor mínimo é 2")]
+        public string Nome { get; set; }
+
+
+        [Range(10, 1000)]
+        [Required(ErrorMessage = "Preencha o Valor")]
+        public decimal Valor { get; set; }
+
+        public bool Gratuito { get; set; }
+        [Required(ErrorMessage = "A descricao é obrigatoria")]
+        [MaxLength(100, ErrorMessage = "O valor maximo é 100")]
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
